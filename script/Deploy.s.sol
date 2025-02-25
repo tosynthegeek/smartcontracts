d621dd79a9433a7e19211e15734d9e217dd528b2
tosynthegeek <oluwatosinojedapo@gmail.com>
2025-02-25 08:41:07 +0100

    feat: deployment script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+import {InsuranceCover} from "../src/InsuranceCover.sol";
+import {InsurancePool} from "../src/InsurancePool.sol";
+import {Vaults} from "../src/Vaults.sol";
+import {Governance} from "../src/Governance.sol";
+import {BqBTC} from "../src/BqBTC.sol";
+
+contract DeployScript is Script {
+    InsuranceCover public insuranceCover;
+    InsurancePool public insurancePool;
+    Vaults public vaults;
+    Governance public governance;
+    BqBTC public bqBTC;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        address OWNER = 0x47C14E2dD82B7Cf0E7426c991225417e4C40Cd19;
+        address ALT_TOKEN = 0x73795572FB8c1c737513156ecb8b1Cc9a3f9cA46;
+        address poolCanister = 0x9DB1bc882935529E8E76abccB1165275BBf8Cbd8;
+        uint256 MIN = 20000000000000;
+
+        bqBTC = new BqBTC("BQ BTC", "BQBTC", 8, MIN, OWNER, ALT_TOKEN, 1000);
+        address btcAddress = address(bqBTC);
+        console.log("BqBTC address: ", btcAddress);
+
+        insurancePool = new InsurancePool(OWNER, btcAddress);
+        address poolAddress = address(insurancePool);
+        console.log("InsurancePool address: ", poolAddress);
+
+        insuranceCover = new InsuranceCover(poolAddress, OWNER, btcAddress);
+        address coverAddress = address(insuranceCover);
+        console.log("InsuranceCover address: ", coverAddress);
+
+        vaults = new Vaults(OWNER, btcAddress);
+        address vaultAddress = address(vaults);
+        console.log("Vaults address: ", vaultAddress);
+
+        governance = new Governance(btcAddress, poolAddress, 2, OWNER);
+        address governanceAddress = address(governance);
+        console.log("Governance address: ", governanceAddress);
+
+        vm.stopBroadcast();
+    }
+}
