219c31aa43683c8ca32f16152a22e13626fece3c
tosynthegeek <oluwatosinojedapo@gmail.com>
2025-03-25 14:09:04 +0100

    chore: imported custom errors

--- src/Governance.sol
+++ src/Governance.sol
@@ -5,7 +5,7 @@ import "openzeppelin-contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-contracts/access/Ownable2Step.sol";
 import "openzeppelin-contracts/utils/ReentrancyGuard.sol";
 import "./CoverLib.sol";
-import "../errors/GovErrors.sol";
+import "./errors/GovErrors.sol";
 
 interface ILP {
     struct Deposits {
@@ -33,16 +33,12 @@ interface IGovToken {
 }
 
 interface ICover {
-    function updateUserCoverValue(
-        address user,
-        uint256 _coverId,
-        uint256 _claimPaid
-    ) external;
+    function updateUserCoverValue(address user, uint256 _coverId, uint256 _claimPaid) external;
 
-    function getUserCoverInfo(
-        address user,
-        uint256 _coverId
-    ) external view returns (CoverLib.GenericCoverInfo memory);
+    function getUserCoverInfo(address user, uint256 _coverId)
+        external
+        view
+        returns (CoverLib.GenericCoverInfo memory);
 }
 
 contract Governance is ReentrancyGuard, Ownable2Step {
@@ -104,12 +100,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         uint256 claimAmount,
         ProposalStaus status
     );
-    event VoteCast(
-        address indexed voter,
-        uint256 indexed proposalId,
-        bool vote,
-        uint256 weight
-    );
+    event VoteCast(address indexed voter, uint256 indexed proposalId, bool vote, uint256 weight);
     event ProposalExecuted(uint256 indexed proposalId, bool approved);
 
     IERC20 public governanceToken;
@@ -119,12 +110,9 @@ contract Governance is ReentrancyGuard, Ownable2Step {
     address public coverContract;
     address public poolContract;
 
-    constructor(
-        address _governanceToken,
-        address _insurancePool,
-        uint256 _votingDuration,
-        address _initialOwner
-    ) Ownable(_initialOwner) {
+    constructor(address _governanceToken, address _insurancePool, uint256 _votingDuration, address _initialOwner)
+        Ownable(_initialOwner)
+    {
         governanceToken = IERC20(_governanceToken);
         tokenContract = IGovToken(_governanceToken);
         lpContract = ILP(_insurancePool);
@@ -138,15 +126,14 @@ contract Governance is ReentrancyGuard, Ownable2Step {
     }
 
     function createProposal(ProposalParams memory params) external {
-        CoverLib.GenericCoverInfo memory userCover = ICoverContract
-            .getUserCoverInfo(params.user, params.coverId);
-        if params.claimAmount > userCover.coverValue {
+        CoverLib.GenericCoverInfo memory userCover = ICoverContract.getUserCoverInfo(params.user, params.coverId);
+        if (params.claimAmount > userCover.coverValue) {
             revert Gov__InsufficientCoverValue();
         }
-        if lpContract.poolActive(params.poolId) == false {
+        if (!lpContract.poolActive(params.poolId)) {
             revert Gov__PoolDoesNotExist();
         }
-        if params.claimAmount <= 0 {
+        if (params.claimAmount <= 0) {
             revert Gov__InvalidClaimAmount();
         }
 
@@ -157,10 +144,8 @@ contract Governance is ReentrancyGuard, Ownable2Step {
             ProposalParams memory param = proposal.proposalParam;
 
             if (
-                param.user == params.user &&
-                param.coverId == params.coverId &&
-                proposal.status != ProposalStaus.Claimed &&
-                proposal.status != ProposalStaus.Rejected
+                param.user == params.user && param.coverId == params.coverId && proposal.status != ProposalStaus.Claimed
+                    && proposal.status != ProposalStaus.Rejected
             ) {
                 revert Gov__CannotCreateProposalForThisCoverNow();
             }
@@ -181,7 +166,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         proposalIds.push(proposalCounter); // Track the proposal ID
 
         bool userExists = false;
-        for (uint i = 0; i < participants.length; i++) {
+        for (uint256 i = 0; i < participants.length; i++) {
             if (participants[i] == msg.sender) {
                 userExists = true;
                 break;
@@ -209,7 +194,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         }
         Proposal storage proposal = proposals[_proposalId];
         if (proposal.createdAt == 0) {
-            rever Gov__ProposalDoesNotExist();
+            revert Gov__ProposalDoesNotExist();
         }
         if (msg.sender == proposal.proposalParam.user) {
             revert Gov__CannotVoteOnOwnProposal();
@@ -230,11 +215,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
             revert Gov__NoVotingWeight();
         }
 
-        voters[_proposalId][msg.sender] = Voter({
-            voted: true,
-            vote: _vote,
-            weight: voterWeight
-        });
+        voters[_proposalId][msg.sender] = Voter({voted: true, vote: _vote, weight: voterWeight});
 
         if (_vote) {
             votesFor[_proposalId].push(msg.sender);
@@ -245,7 +226,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         }
 
         bool userExists = false;
-        for (uint i = 0; i < participants.length; i++) {
+        for (uint256 i = 0; i < participants.length; i++) {
             if (participants[i] == msg.sender) {
                 userExists = true;
                 break;
@@ -265,11 +246,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
             uint256 proposalId = proposalIds[i];
             Proposal storage proposal = proposals[proposalId];
 
-            if (
-                proposal.status == ProposalStaus.Pending &&
-                block.timestamp > proposal.deadline &&
-                !proposal.executed
-            ) {
+            if (proposal.status == ProposalStaus.Pending && block.timestamp > proposal.deadline && !proposal.executed) {
                 proposal.executed = true;
                 proposal.timeleft = 0;
 
@@ -277,9 +254,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
                     proposals[proposalId].status = ProposalStaus.Approved;
                     address[] memory correctVoters = votesFor[proposalId];
                     ICoverContract.updateUserCoverValue(
-                        proposal.proposalParam.user,
-                        proposal.proposalParam.coverId,
-                        proposal.proposalParam.claimAmount
+                        proposal.proposalParam.user, proposal.proposalParam.coverId, proposal.proposalParam.claimAmount
                     );
 
                     for (uint256 j = 0; j < correctVoters.length; j++) {
@@ -301,13 +276,8 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         }
     }
 
-    function updateProposalStatusToClaimed(
-        uint256 proposalId
-    ) public nonReentrant {
-        if (
-            msg.sender != proposals[proposalId].proposalParam.user &&
-            msg.sender != poolContract
-        ) {
+    function updateProposalStatusToClaimed(uint256 proposalId) public nonReentrant {
+        if (msg.sender != proposals[proposalId].proposalParam.user && msg.sender != poolContract) {
             revert Gov__NotValidProposer();
         }
 
@@ -325,15 +295,11 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         return proposalCounter;
     }
 
-    function getProposalDetails(
-        uint256 _proposalId
-    ) external returns (Proposal memory) {
+    function getProposalDetails(uint256 _proposalId) external returns (Proposal memory) {
         if (block.timestamp >= proposals[_proposalId].deadline) {
             proposals[_proposalId].timeleft = 0;
         } else {
-            proposals[_proposalId].timeleft =
-                (proposals[_proposalId].deadline - block.timestamp) /
-                1 days;
+            proposals[_proposalId].timeleft = (proposals[_proposalId].deadline - block.timestamp) / 1 days;
         }
         return proposals[_proposalId];
     }
@@ -345,9 +311,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
             if (block.timestamp >= result[i].deadline) {
                 result[i].timeleft = 0;
             } else {
-                result[i].timeleft =
-                    (result[i].deadline - block.timestamp) /
-                    1 days;
+                result[i].timeleft = (result[i].deadline - block.timestamp) / 1 days;
             }
         }
         return result;
@@ -356,10 +320,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
     function getActiveProposals() public view returns (Proposal[] memory) {
         uint256 activeCount = 0;
         for (uint256 i = 0; i < proposalIds.length; i++) {
-            if (
-                proposals[proposalIds[i]].deadline == 0 ||
-                proposals[proposalIds[i]].deadline > block.timestamp
-            ) {
+            if (proposals[proposalIds[i]].deadline == 0 || proposals[proposalIds[i]].deadline > block.timestamp) {
                 activeCount++;
             }
         }
@@ -367,20 +328,15 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         Proposal[] memory result = new Proposal[](activeCount);
         uint256 index = 0;
         for (uint256 i = 0; i < proposalIds.length; i++) {
-            if (
-                proposals[proposalIds[i]].deadline == 0 ||
-                proposals[proposalIds[i]].deadline >= block.timestamp
-            ) {
+            if (proposals[proposalIds[i]].deadline == 0 || proposals[proposalIds[i]].deadline >= block.timestamp) {
                 result[index] = proposals[proposalIds[i]];
                 if (
-                    block.timestamp == result[index].deadline ||
-                    proposals[proposalIds[i]].status == ProposalStaus.Submitted
+                    block.timestamp == result[index].deadline
+                        || proposals[proposalIds[i]].status == ProposalStaus.Submitted
                 ) {
                     result[index].timeleft = 0;
                 } else {
-                    result[index].timeleft =
-                        (result[index].deadline - block.timestamp) /
-                        1 days;
+                    result[index].timeleft = (result[index].deadline - block.timestamp) / 1 days;
                 }
 
                 index++;
@@ -393,8 +349,8 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         uint256 pastCount = 0;
         for (uint256 i = 0; i < proposalIds.length; i++) {
             if (
-                proposals[proposalIds[i]].status != ProposalStaus.Submitted &&
-                proposals[proposalIds[i]].deadline < block.timestamp
+                proposals[proposalIds[i]].status != ProposalStaus.Submitted
+                    && proposals[proposalIds[i]].deadline < block.timestamp
             ) {
                 pastCount++;
             }
@@ -403,8 +359,8 @@ contract Governance is ReentrancyGuard, Ownable2Step {
         uint256 index = 0;
         for (uint256 i = 0; i < proposalIds.length; i++) {
             if (
-                proposals[proposalIds[i]].status != ProposalStaus.Submitted &&
-                proposals[proposalIds[i]].deadline < block.timestamp
+                proposals[proposalIds[i]].status != ProposalStaus.Submitted
+                    && proposals[proposalIds[i]].deadline < block.timestamp
             ) {
                 result[index] = proposals[proposalIds[i]];
                 result[index].timeleft = 0;
@@ -442,7 +398,7 @@ contract Governance is ReentrancyGuard, Ownable2Step {
     }
 
     function updateRewardAmount(uint256 numberofTokens) public onlyAdmin {
-        if numberofTokens <= 0 {
+        if (numberofTokens <= 0) {
             revert Gov__InvalidRewardAmount();
         }
         REWARD_AMOUNT = numberofTokens * 10 ** 18;
